9b1ae9b8aaf1d2e17cd71c69d6adaf46708128ef
gundamgn000 <109073563+gundamgn000@users.noreply.github.com>
2023-01-06 13:05:52 +0800

    Add files via upload

---
+++ articles/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render
+from .scrapers import Pixnet
+
+
+def index(request):
+
+    pixnet = Pixnet(None)
+
+    if request.method == "POST":
+        pixnet = Pixnet(request.POST.get("restaurant_name"))  # 取得查詢條件
+
+    context = {
+        "articles": pixnet.get_articles()
+    }
+
+    return render(request, "articles/index.html", context)
